3dec09a9a0c22958d848e6ab7cefd21fca183809
samst <samstitt@gmail.com>
2023-04-03 11:19:27 +0800

    Initial commit

---
+++ Translate.py
@@ -0,0 +1,142 @@
+import streamlit as st
+import openai
+import time
+import pandas as pd
+import pyperclip
+
+
+def initiate_states():
+
+    # Create default session states
+    if 'messages' not in ss:
+        ss['messages'] = []
+
+    if 'state' not in ss:
+        ss['state'] = "Intro"
+
+    if 'model_reply' not in ss:
+        ss['model_reply'] = ""
+
+    if 'user_reply' not in ss:
+        ss['user_reply'] = ""
+
+    if 'current_topic' not in ss:
+        ss['current_topic'] = {}
+
+    if 'topics' not in ss:
+        ss['topics'] = {}
+
+    if 'counts' not in ss:
+        ss['counts'] = 1
+
+    if 'user_info' not in ss:
+        ss['user_info'] = {}
+
+    if 'load_questions' not in ss:
+        ss['load_questions'] = False
+
+    if 'text_to_translate' not in ss:
+        ss['text_to_translate'] = ""
+
+    df = pd.read_excel('presets.xlsx', engine='openpyxl', index_col=0)
+    df.fillna('', inplace=True)
+    if 'presets' not in ss:
+        ss['presets'] = df
+
+    if 'current_preset' not in ss:
+        ss['current_preset'] = 'New...'
+
+
+def translate():
+    ss.model_reply = ""
+
+
+def update_model_response():
+    """Calls the OpenAI API and updates model_response_display"""
+    openai.api_key = st.secrets['SECRET_KEY']
+
+    qu_attempts = 1
+    while qu_attempts <= 10:
+        print(ss.messages)
+
+        try:
+            response = []
+            for resp in openai.ChatCompletion.create(
+                    model="gpt-4",
+                    messages=ss.messages,
+                    stream=True):
+                if 'content' in resp['choices'][0]['delta']:
+                    response.append(resp['choices'][0]['delta']['content'])
+                    result = "".join(response).strip()
+                    model_response_display.markdown(f'{result}')
+
+            ss.model_reply = "".join(response).strip()
+            qu_attempts = 11
+
+        except:
+            print(f"openai error, attempt {qu_attempts}")
+            qu_attempts += 1
+            time.sleep(2)
+
+    st.experimental_rerun()
+
+
+# Initiate states and variables
+st.set_page_config(page_title="Translate | Yellow Door Studio", page_icon="yellow_door_favicon.png", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
+
+ss = st.session_state
+initiate_states()
+
+st.image('Yellow Door Studio.webp', width=250)
+st.header('Brand Translator')
+st.markdown('Select translation preset and enter text to be translated in the area below.')
+
+
+options = ss.presets.index.tolist()
+options.pop(0)
+
+try:
+    index = options.index(ss.current_preset)
+except ValueError:
+    index = 0
+
+ss.current_preset = st.selectbox('Load preset:', options=options, index=index)
+
+
+with st.expander('Translation prompt', expanded=False):
+    st.markdown(ss.presets.loc[ss.current_preset, 'prompt'])
+
+ss.text_to_translate = st.text_area("Enter text to translate:", value=ss.text_to_translate)
+
+ss.messages = [
+    {"role": "system", "content": ss.presets.loc[ss.current_preset, 'prompt']},
+    {"role": "user", "content": ss.text_to_translate}
+]
+
+if ss.model_reply == "":
+    model_response_display = st.empty()
+    if ss.text_to_translate != "":
+        update_model_response()
+else:
+    model_response_display = st.text_area('Translation:', value=ss.model_reply)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.button('Translate', on_click=translate, type='primary')
+
+with col2:
+    if st.button('Clear'):
+        ss.model_reply = ""
+        ss.text_to_translate = ""
+        st.experimental_rerun()
+
+with col3:
+    if st.button('Copy translation'):
+        try:
+            pyperclip.copy(model_response_display)
+            st.write("Copied to clipboard")
+
+        except:
+            st.write('Cannot copy')
---
+++ pages/Manage presets.py
@@ -0,0 +1,123 @@
+import streamlit as st
+import pandas as pd
+
+
+def initialise_states():
+
+    df = pd.read_excel('presets.xlsx', engine='openpyxl', index_col=0)
+    df.fillna('', inplace=True)
+    ss['presets'] = df
+
+    if 'current_preset' not in ss:
+        ss['current_preset'] = 'New…'
+
+
+def save_preset():
+    new_values = {'from_language': from_language,
+                  'to_language': to_language,
+                  'context': context,
+                  'expert_persona': expert_persona,
+                  'tone': tone,
+                  'brand_voice': brand_voice,
+                  'audience': audience,
+                  'style': style,
+                  'audience_region': audience_region,
+                  'translation_guide': translation_guide,
+                  'prompt': prompt}
+
+    ss.presets.loc[preset_name] = pd.Series(new_values)
+    ss.presets.to_excel('presets.xlsx', engine='openpyxl')
+
+    df = pd.read_excel('presets.xlsx', engine='openpyxl', index_col=0)
+    ss['presets'] = df
+    ss.current_preset = preset_name
+
+
+def delete_preset(preset_name):
+    ss.presets.drop(preset_name, inplace=True)
+    ss.presets.to_excel('presets.xlsx', engine='openpyxl')
+
+    df = pd.read_excel('presets.xlsx', engine='openpyxl', index_col=0)
+    ss['presets'] = df
+
+
+st.set_page_config(page_title="Presets | Yellow Door Studio", page_icon="yellow_door_favicon.png", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
+
+st.image('Yellow Door Studio.webp', width=250)
+st.header('Manage Presets')
+
+ss = st.session_state
+initialise_states()
+
+options = ss.presets.index.tolist()
+index = options.index(ss.current_preset)
+
+ss.current_preset = st.selectbox('Select preset to edit or add new:', options=options, index=index)
+
+with st.form("preset_form"):
+    st.markdown('#### Preset settings')
+    preset_name = st.text_input('Preset name:', ss.current_preset)
+    from_language = st.text_input('Language to translate from:', placeholder='e.g. English', value=ss.presets.loc[ss.current_preset, 'from_language'])
+    to_language = st.text_input('Language to translate to:', placeholder='e.g. Traditional Chinese', value=ss.presets.loc[ss.current_preset, 'to_language'])
+    context = st.text_input('Context:', placeholder='e.g. The text is being translated for a company report', value=ss.presets.loc[ss.current_preset, 'context'])
+    expert_persona = st.text_input('Persona of writer:', placeholder='e.g. expert presentation copywriter', value=ss.presets.loc[ss.current_preset, 'expert_persona'])
+    tone = st.text_input('Tone:', placeholder='e.g. positive and friendly', value=ss.presets.loc[ss.current_preset, 'tone'])
+    brand_voice = st.text_input('Brand voice:', placeholder='e.g. positive, friendly, inspiring and motivating', value=ss.presets.loc[ss.current_preset, 'brand_voice'])
+    style = st.text_input('Style:', placeholder='e.g. Informal, chatty', value=ss.presets.loc[ss.current_preset, 'style'])
+    audience = st.text_input('Audience:', placeholder='Company employees', value=ss.presets.loc[ss.current_preset, 'audience'])
+    audience_region = st.text_input('Audience region:', placeholder='e.g. Hong Kong', value=ss.presets.loc[ss.current_preset, 'audience_region'])
+
+    translation_guide = st.text_area('Translation guide:',
+                                     placeholder='Enter any specific phrases or terms you want to '
+                                     'use in this type of translation',
+                                     value=ss.presets.loc[ss.current_preset, 'translation_guide'])
+
+    prompt = f"""
+    Act as a translator from {from_language} to {to_language}.
+
+    When providing the translation, take on the persona of {expert_persona}.
+    {context}. The writing style is {style}, targeted towards {audience} in {audience_region}.
+    The tone should be {tone}, taking into consideration the brand voice: {brand_voice}.
+
+    Adapt the sentence structure and order of text to read naturally in {to_language}. 
+
+    ---
+
+    Use the following translations for similar words or phrases:
+
+    {translation_guide}
+
+    ---
+
+    When I provide you with {from_language} text, reply with the translation.
+    """
+
+    with st.expander('Translation prompt', expanded=False):
+        st.markdown(prompt)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.form_submit_button("Save preset", type='primary'):
+            if preset_name != "New…":
+                save_preset()
+                st.write("Preset saved")
+                ss.current_preset = preset_name
+                st.experimental_rerun()
+            else:
+                st.warning('Please update the preset name')
+
+    with col2:
+        if st.form_submit_button("Delete preset"):
+            print(ss.current_preset)
+            if ss.current_preset != "New…":
+                delete_preset(ss.current_preset)
+                st.write("Preset removed")
+                initialise_states()
+                ss.current_preset = ss.presets.index[0] if not ss.presets.empty else ""
+                st.experimental_rerun()
+
+
+
+
